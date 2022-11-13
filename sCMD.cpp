4dbf941b789c5d207ffbee34cde4bae5ed99a98c
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2022-11-13 01:41:32 +0100

    First commit.

---
+++ sCMD.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file sCMD.cpp
+ * @author silvio3105 (www.github.com/silvio3105)
+ * @brief Simple Command Handler source file.
+ * @version v0.1
+ * @date 13.11.2022
+ * 
+ * @copyright Copyright (c) 2022, silvio3105
+ * 
+ */
+
+
+/*
+License
+
+Copyright (c) 2022, silvio3105 (www.github.com/silvio3105)
+
+Access and use of this Project and its contents are granted free of charge to any Person.
+The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+Commercial use of this Project and its contents is prohibited.
+Modifying this License and/or sublicensing is prohibited.
+
+THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+This License shall be included in all methodal textual files.
+*/
+
+
+// ----- INCLUDE FILES
+#include			"sCMD.h"
+
+
+// END WITH NEW LINE
---
+++ sCMD.h
@@ -0,0 +1,47 @@
+/**
+ * @file sCMD.h
+ * @author silvio3105 (www.github.com/silvio3105)
+ * @brief Simple Command Handler header file.
+ * @version v0.1
+ * @date 13.11.2022
+ * 
+ * @copyright Copyright (c) 2022, silvio3105
+ * 
+ */
+
+
+/*
+License
+
+Copyright (c) 2022, silvio3105 (www.github.com/silvio3105)
+
+Access and use of this Project and its contents are granted free of charge to any Person.
+The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+Commercial use of this Project and its contents is prohibited.
+Modifying this License and/or sublicensing is prohibited.
+
+THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+This License shall be included in all methodal textual files.
+*/
+
+
+#ifndef _SCMD_H_
+#define _SCMD_H_
+
+// ----- INCLUDE FILES
+#include            "sStd.h"
+
+/** \addtogroup sCMD 
+ * Simple Command Handler
+ * @{
+*/
+
+
+/** @}*/
+
+#endif // _SCMD_H_
+
+// END WITH NEW LINE
